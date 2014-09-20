97ffc78234177734ada59487eca6802d187e8c0d
RenanAngmar <rlsdeacon@gmail.com>
2014-09-20 15:57:02 -0400

    Codigo fonte SYS-Pizza

---
+++ CodigoFonte/WEB/SYS-Pizza/src/negocio/Cliente.java
@@ -0,0 +1,66 @@
+package negocio;
+
+public class Cliente {
+	
+	private Long codigo;
+	private String nome;
+	private String rg;
+	private String cpf;
+	private String endereco;
+	private String telefone;
+	
+	
+	
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+
+}
